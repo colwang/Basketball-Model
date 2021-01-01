3c09296c12feb83d282eeb151c552eb1c6f571a0
Colin Wang <colwang@umich.edu>
2021-01-01 13:23:08 -0500

    Added more net layers

--- ML_net.py
+++ ML_net.py
@@ -10,13 +10,21 @@ class Net(nn.Module):
 		self.fc1 = nn.Linear(1*8, 64)
 		self.fc2 = nn.Linear(64, 64)
 		self.fc3 = nn.Linear(64, 64)
-		self.fc4 = nn.Linear(64, 2)
+		self.fc4 = nn.Linear(64, 64)
+		self.fc5 = nn.Linear(64, 64)
+		self.fc6 = nn.Linear(64, 64)
+		self.fc7 = nn.Linear(64, 64)
+		self.fc8 = nn.Linear(64, 2)
 
 	def forward(self, x):
 		x = F.relu(self.fc1(x))
 		x = F.relu(self.fc2(x))
 		x = F.relu(self.fc3(x))
-		x = self.fc4(x)
+		x = F.relu(self.fc4(x))
+		x = F.relu(self.fc5(x))
+		x = F.relu(self.fc6(x))
+		x = F.relu(self.fc7(x))
+		x = self.fc8(x)
 		return F.log_softmax(x, dim=1)
 
 net = Net()
